da32db2b10282e0ac074522424883019b89aadb0
ericolivalura <eric.oliveira@alura.com.br>
2024-02-08 11:05:59 -0300

    iniciando la base de datos parte 2

--- src/main/java/com/aluracursos/desafio/model/Autor.java
+++ src/main/java/com/aluracursos/desafio/model/Autor.java
@@ -1,4 +1,41 @@
 package com.aluracursos.desafio.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "autores")
 public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private LocalDate fechaDeNacimiento;
+
+    @ManyToOne
+    private Libro libro;
+
+    public Autor(List<DatosAutor> datosAutor) {
+        this.nombre = String.valueOf(datosAutor.get(0));
+        this.fechaDeNacimiento = datosAutor.get(1);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(String fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
 }
--- src/main/java/com/aluracursos/desafio/model/Libro.java
+++ src/main/java/com/aluracursos/desafio/model/Libro.java
@@ -5,10 +5,55 @@ import jakarta.persistence.*;
 import java.util.List;
 
 @Entity
+@Table(name = "libros")
 public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titulo;
-    private List<Autor> autores;
+    @OneToMany(mappedBy = "libro", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Autor autor;
     private List<String> idiomas;
     private Double numeroDeDescargas;
+
+    public Libro(DatosLibros datosLibros) {
+        this.titulo = datosLibros.titulo();
+        this.autor = new Autor(datosLibros.autor());
+        this.idiomas = datosLibros.idiomas();
+        this.numeroDeDescargas = datosLibros.numeroDeDescargas();
+    }
+
+    public Libro() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public List<String> getIdiomas() {
+        return idiomas;
+    }
+
+    public Double getNumeroDeDescargas() {
+        return numeroDeDescargas;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", autor=" + autor.getNombre() +
+                ", idiomas=" + idiomas +
+                ", numeroDeDescargas=" + numeroDeDescargas +
+                '}';
+    }
 }
