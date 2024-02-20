b2c718fca8afced8d5f3afbbb47fafba9c1eea77
ericolivalura <eric.oliveira@alura.com.br>
2024-02-20 15:52:14 -0300

    simplificando la aplicacion

--- src/main/java/com/aluracursos/desafio/principal/Principal.java
+++ src/main/java/com/aluracursos/desafio/principal/Principal.java
@@ -28,15 +28,10 @@ public class Principal {
             ------------
             Elija la opción a través de su número:
             1- buscar libro por título
-            2- top 10 libros más descargados
-            3- exhibir estadísticas de descargas totales
-            4- buscar un autor
-            5- listar libros registrados
-            6- listar autores registrados
-            7- listar autores por año de nacimiento
-            8- listar autores por intervalo de años
-            9- listar autores vivos por año
-            10- exhibir estadísticas de descargas de los libros registrados
+            2- listar libros registrados
+            3- listar autores registrados
+            4- listar autores vivos en un determinado año
+            5- exhibir estadísticas de descargas de los libros registrados
             0 - salir
             """;
 
@@ -55,20 +50,14 @@ public class Principal {
             teclado.nextLine();
             switch (opcionElegida) {
                 case 1 -> buscarLibroPorTitulo();
-                case 2 -> listarTop10LibrosMasDescargados(); //extra
-                case 3 -> exhibirEstadisticasDeDescargas(); //extra
-                case 4 -> buscarAutor(); //extra
-                case 5 -> listarLibrosRegistrados();
-                case 6 -> listarAutoresRegistrados();
-                case 7 -> listarAutoresPorAnoDeNacimiento(); //extra
-                case 8 -> listarAutoresPorIntervaloDeAnos(); //extra
-                case 9 -> listarAutoresVivosEnAnoEspecifico();
-                case 10 -> exhibirEstadisticasDeDescargasDeLosLibrosRegistrados();
+                case 2 -> listarLibrosRegistrados();
+                case 3 -> listarAutoresRegistrados();
+                case 4 -> listarAutoresVivosEnAnoEspecifico();
+                case 5 -> exhibirEstadisticasDeDescargasDeLosLibrosRegistrados();
                 case 0 -> System.out.println("Hasta luego...");
                 default -> System.out.println("Opcion invalida");
             }
         }
-
     }
 
     private void buscarLibroPorTitulo() {
@@ -113,47 +102,7 @@ public class Principal {
         } else {
             System.out.println("Campo de texto vacío, por favor, inténtelo de nuevo e ingrese un texto válido.");
         }
-
         return null;
-    }
-
-    private void listarTop10LibrosMasDescargados() {
-        System.out.println("Top 10 libros más descargados");
-
-        Datos datos = getDadosWeb();
-        datos.resultados().stream()
-                .sorted(Comparator.comparing(DatosLibros::numeroDeDescargas).reversed())
-                .limit(10)
-                .map(l -> l.titulo().toUpperCase())
-                .forEach(System.out::println);
-    }
-
-    private void exhibirEstadisticasDeDescargas() {
-        Datos datos = getDadosWeb();
-        DoubleSummaryStatistics est = datos.resultados().stream()
-                .filter(d -> d.numeroDeDescargas() > 0)
-                .collect(Collectors.summarizingDouble(DatosLibros::numeroDeDescargas));
-        System.out.println("Cantidad media de descargas: " + est.getAverage());
-        System.out.println("Cantidad máxima de descargas: " + est.getMax());
-        System.out.println("Cantidad mínima de descargas: " + est.getMin());
-        System.out.println("Cantidad de registros evaluados para calcular las estadisticas: " + est.getCount());
-    }
-
-    private void buscarAutor() {
-        System.out.println("Ingrese el nombre del autor que desea buscar");
-        var nombreAutor = teclado.nextLine();
-
-        if (!nombreAutor.isBlank()) {
-            List<Autor> autores = autorRepositorio.findAllByNombreContainingIgnoreCase(nombreAutor);
-
-            if (!autores.isEmpty()) {
-                autores.forEach(System.out::println);
-            } else {
-                System.out.println("No existen autores con este nombre en nuestra base de datos");
-            }
-        } else {
-            System.out.println("Campo de texto vacío, por favor, inténtelo de nuevo e ingrese un texto válido.");
-        }
     }
 
     private void listarLibrosRegistrados() {
@@ -164,49 +113,7 @@ public class Principal {
         autorRepositorio.findAll().forEach(System.out::println);
     }
 
-    private Datos getDadosWeb() {
-        return conversor.obtenerDatos(json, Datos.class);
-    }
-
     /* Consultas con fechas (Autores) */
-
-    private void listarAutoresPorAnoDeNacimiento() {
-        System.out.println("Ingrese el año de nacimiento del autor(es) que desea buscar");
-        var anoNacimiento = teclado.nextLine();
-
-        if (!anoNacimiento.isBlank()) {
-            List<Autor> autoresBuscados = autorRepositorio.findByFechaDeNacimiento(Integer.valueOf(anoNacimiento));
-
-            if (!autoresBuscados.isEmpty()) {
-                autoresBuscados.forEach(autor -> System.out.println(autor.toString()));
-            } else {
-                System.out.println("No se han encontrado autores nacidos este año.");
-            }
-        } else {
-            System.out.println("Campo de texto vacío, por favor, inténtelo de nuevo e ingrese un numero entero válido.");
-        }
-    }
-
-    private void listarAutoresPorIntervaloDeAnos() {
-        System.out.println("Ingrese el año inicial del intervalo");
-        var anoInicial = teclado.nextLine();
-
-        System.out.println("Ingrese el año final del intervalo");
-        var anoFinal = teclado.nextLine();
-
-        if (!(anoInicial.isBlank() || anoFinal.isBlank())) {
-            List<Autor> autoresBuscados = autorRepositorio.findByFechaDeNacimientoGreaterThanEqualAndFechaDeFallecimientoLessThan(
-                    Integer.valueOf(anoInicial), Integer.valueOf(anoFinal));
-
-            if (!autoresBuscados.isEmpty()) {
-                autoresBuscados.forEach(autor -> System.out.println(autor.toString()));
-            } else {
-                System.out.println("No se han encontrado autores vivos en este intervalo de años.");
-            }
-        } else {
-            System.out.println("Campo(s) de texto vacío(s), por favor, inténtelo de nuevo e ingrese un numero entero válido en cada campo.");
-        }
-    }
 
     private void listarAutoresVivosEnAnoEspecifico() {
         System.out.println("Ingrese el año vivo de autor(es) que desea buscar");
--- src/main/java/com/aluracursos/desafio/repository/AutorRepository.java
+++ src/main/java/com/aluracursos/desafio/repository/AutorRepository.java
@@ -7,9 +7,6 @@ import java.util.List;
 
 public interface AutorRepository extends JpaRepository<Autor, Long> {
     Autor findByNombre(String nombre);
-    List<Autor> findByFechaDeNacimiento(Integer fechaDeNacimiento);
-    List<Autor> findByFechaDeNacimientoGreaterThanEqualAndFechaDeFallecimientoLessThan(Integer fechaDeNacimiento, Integer fechaDeFallecimiento);
     List<Autor> findByFechaDeNacimientoLessThanEqualAndFechaDeFallecimientoGreaterThan(Integer fechaDeNacimiento, Integer fechaDeFallecimiento);
-    List<Autor> findAllByNombreContainingIgnoreCase(String nombreAutor);
 
 }
